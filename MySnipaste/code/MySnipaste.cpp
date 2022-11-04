5a5944f68f620f4aa15bdf966b2f2a3c2ed3a128
shuaiyang <1138344933@qq.com>
2022-11-04 16:51:45 +0800

    modify the title

--- MySnipaste/code/MySnipaste.cpp
+++ MySnipaste/code/MySnipaste.cpp
@@ -7,11 +7,14 @@ MySnipaste::MySnipaste(QWidget *parent)
 {
     ui.setupUi(this);
 
-
+	this->setWindowTitle("MySnipaste");
+	this->setWindowIcon(QIcon(":/MySnipaste/resource/title.png"));
 	//ui.label_tip->setText(QStringLiteral("回车键（Enter）确定\n"));
 	this->statusBar()->setStyleSheet("QStatusBar::item{border: 0px}");
 	//this->statusBar->setToolTip(QStringLiteral("回车键（Enter）确定截图"));
-	this->statusBar()->addPermanentWidget(new QLabel("回车键（Enter）确定截图"));//设置statusBar永久提示部件
+	this->statusBar()->addPermanentWidget(new QLabel("回车键（Enter）确定截图/退出键（Escape）退出截图"));//设置statusBar永久提示部件
+	//this->statusBar()->addPermanentWidget(new QLabel("退出键（Escape）退出截图"));//设置statusBar永久提示部件
+
 	//QMetaObject::connectSlotsByName(this);
 	//capScn = new CaptureScreen();
 	connect(ui.pushButton, &QPushButton::clicked, this, &MySnipaste::onStart);
